595cd0d66eab15071c7d21752529a52af786a2a5
Marko Nikolic <marko.supergun@gmail.com>
2022-03-06 23:28:45 +0100

    fix update3

--- popup.js
+++ popup.js
@@ -20,13 +20,7 @@ document.body.onload = function () {
         document.body.appendChild(passwordsDiv);
     })
 
-    document.querySelectorAll(".dropdown-toggle").forEach(function (v) {
-        const id = v.getAttribute("data-drop-id");
-        v.onclick = function () {
-            dropdown(id);
-        };
 
-    });
 
     const dropdown = (id) => {
         const button = document.querySelector(`.dropdown-toggle[data-drop-id="${id}"]`),
@@ -42,23 +36,83 @@ document.body.onload = function () {
         }
     }
 
+    const dropdownGenerator = async (d_tr3, i, passwords, linkEl, deleteBtn) => {
+        const div = document.createElement("div"),
+            div_menu = document.createElement("div"),
+            button = document.createElement("button"),
+            hi = document.createElement("i"),
+            menus = [
+                {
+                    id: 0,
+                    what: "del",
+                    icon:"fas fa-trash-alt",
+                    title:"Delete password"
+                }
+            ];
+        hi.setAttribute("class", "fas fa-ellipsis-v");
+        div.classList.add("dropdown");
+        div.classList.add("forSearch");
+        div_menu.classList.add("dropdown-menu");
+        button.classList.add("dropdown-toggle");
+        button.type = "button";
+        button.id = "dropdownMenuButton";
+        button.setAttribute("aria-haspopup", "true");
+        button.setAttribute("data-drop-id", i);
+        button.setAttribute("aria-expanded", "false");
+        button.setAttribute("title", "Options");
+        button.appendChild(hi);
+        div_menu.setAttribute("class", "dropdown-menu");
+        div_menu.setAttribute("data-drop-id", i);
+        div_menu.setAttribute("aria-labelledby", "dropdownMenuButton");
+        div.appendChild(button);
+        menus.forEach(function (h) {
+            const a = document.createElement("a");
+            // <a class="dropdown-item" href="#">Action</a>
+            a.setAttribute("class", "dropdown-item");
+            if (h.what == "del") {
+                var i = document.createElement("i");
+                i.setAttribute("class",`${h.icon}`);
+
+                a.appendChild(i);
+                a.innerHTML += h.title;
+                a.addEventListener("click", function () {
+                    const updatedPasswords = passwords.filter(pwd => pwd !== password);
+                    chrome.storage.sync.set({ passwords: updatedPasswords });
+
+                    linkEl.remove();
+                    deleteBtn.remove();
+                });
+            }
+            div_menu.appendChild(a);
+        });
+
+        div.appendChild(div_menu);
+
+        d_tr3.appendChild(div);
+    }
+
     const getUrls = async () => {
-        //const { passwords } = await chrome.storage.sync.get("passwords");
+        // const { passwordk } = await chrome.storage.sync.get("passwords");
+        var i = 0;
         const passwords = [{
             password: "amefk323 pass",
             url: "https://www.youtube.com/watch?v=tl7gaL2me8k&list=RDtl7gaL2me8k&start_radio=1"
         }]
         passwords.forEach(password => {
+
             const d_tr = document.createElement("tr"),
                 d_tr1 = document.createElement("td"),
                 d_tr2 = document.createElement("td"),
+                d_tr2_div = document.createElement("div"),
                 d_tr2_btn = document.createElement("i"),
                 d_tr3 = document.createElement("td"),
                 d_tr4 = document.createElement("td"),
                 d_tr5 = document.createElement("td"),
+                Eyes_I = document.createElement("i");
                 favicon = document.createElement("img");
 
-
+            Eyes_I.setAttribute("class","fas fa-eye show_hide_password");
+            Eyes_I.title = "Show/Hide password";
 
             const linkEl = document.createElement("a");
             linkEl.href = password.url;
@@ -67,15 +121,23 @@ document.body.onload = function () {
             const deleteBtn = document.createElement("button");
             deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
 
+            d_tr2_div.classList.add("div_options");
+            d_tr2_div.appendChild(deleteBtn);
+
+            // d_tr3.appendChild(
+            d_tr3.appendChild(Eyes_I);
+            dropdownGenerator(d_tr3, i, passwords, linkEl, deleteBtn);
+
+
             favicon.src = "http://www.google.com/s2/favicons?domain=" + password.url;
 
             d_tr1.appendChild(favicon);
             d_tr1.appendChild(linkEl);
-            d_tr2.appendChild(deleteBtn);
-
+            d_tr2.appendChild(d_tr2_div);
+            d_tr.setAttribute("data-int", i);
             d_tr.setAttribute("data-id", "is_value");
             d_tr.appendChild(d_tr1);
-            d_tr.appendChild(d_tr2);
+          //  d_tr.appendChild(d_tr2);
             d_tr.appendChild(d_tr3);
             //passwordsDiv.appendChild(linkEl);
             //  passwordsDiv.appendChild(deleteBtn);
@@ -86,7 +148,16 @@ document.body.onload = function () {
 
                 linkEl.remove();
                 deleteBtn.remove();
-            })
+            });
+
+            document.querySelectorAll(".dropdown-toggle").forEach(function (v) {
+                const id = v.getAttribute("data-drop-id");
+                v.onclick = function () {
+                    dropdown(id);
+                };
+
+            });
+            i++;
         })
     }
 
